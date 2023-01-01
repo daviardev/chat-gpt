978d2adbb75159df78b6b21601bf3bc9b05f5fb0
devsilva <jerson.silva99@hotmail.com>
2023-01-01 16:24:30 -0500

    Add icon send

---
+++ src/components/Icons/Send.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Send = (props) => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth={0}
+    viewBox="0 0 20 20"
+    className="h-4 w-4 rotate-90"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M10.894 2.553a1 1 0 0 0-1.788 0l-7 14a1 1 0 0 0 1.169 1.409l5-1.429A1 1 0 0 0 9 15.571V11a1 1 0 1 1 2 0v4.571a1 1 0 0 0 .725.962l5 1.428a1 1 0 0 0 1.17-1.408l-7-14z"
+      stroke="none"
+    />
+  </svg>
+)
+
+export default Send
