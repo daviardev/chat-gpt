b9a39805d8d7c32dc814bab7e3d5e503513ab5f7
devsilva <jerson.silva99@hotmail.com>
2023-01-01 17:51:44 -0500

    Create chat with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Plus from './components/Icons/Plus'
+import Icon from './components/Icons/Icon'
 
 import './App.css'
 import './normal.css'
@@ -17,6 +18,28 @@ function App() {
         </div>
       </aside>
       <section className='chat__box'>
+        <div className="chat_log">
+          <div className="chat__message">
+            <div className="chat__message_center">
+              <div className="avatar">
+
+              </div>
+              <div className="message">
+                Como se hace un console.log en js
+              </div>
+            </div>
+          </div>
+          <div className="chat__message chatgpt">
+            <div className="chat__message_center">
+              <div className="avatar">
+                <Icon />
+              </div>
+              <div className="message">
+                console.log("Hola mundo");
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="chat__input_holder">
           <textarea className='chat__input_textarea' rows='1' />
         </div>
