d1367851e9b4b92bd1e602e19c97e74f88ac7102
devsilva <jerson.silva99@hotmail.com>
2023-01-01 14:53:09 -0500

    Create structure of the aside menu

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  return <>
+    <div className="App">
+      <aside className="side__menu">
+        <h1>Aside</h1>
+      </aside>
+      <section className='chat__box'>
+        
+      </section>
+    </div>
+  </>
+}
+
+export default App
