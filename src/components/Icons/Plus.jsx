36cee2c9676a6834c5fe41afec2fd1e4d68b73a1
devsilva <jerson.silva99@hotmail.com>
2023-01-01 15:41:36 -0500

    Convert svg plus to react component

---
+++ src/components/Icons/Plus.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Plus = (props) => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="h-4 w-4"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M12 5v14M5 12h14" />
+  </svg>
+)
+
+export default Plus
